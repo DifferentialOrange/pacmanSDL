54fad389db1fdf8e0dee573c28fb570c84748c87
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2020-01-26 22:24:15 +0300

    Remove excessive function

--- src/utils.cpp
+++ src/utils.cpp
@@ -26,20 +26,6 @@ SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren)
 }
 
 /**
-* Draw an SDL_Texture to an SDL_Renderer at some destination rect
-* taking a clip of the texture if desired
-* @param tex The source texture we want to draw
-* @param ren The renderer we want to draw to
-* @param dst The destination rectangle to render the texture to
-* @param clip The sub-section of the texture to draw (clipping rect)
-*       default of nullptr draws the entire texture
-*/
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, SDL_Rect dst, SDL_Rect *clip = nullptr)
-{
-    SDL_RenderCopy(ren, tex, clip, &dst);
-}
-
-/**
 * Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
 * the texture's width and height and taking a clip of the texture if desired
 * If a clip is passed, the clip's width and height will be used instead of
@@ -62,5 +48,6 @@ void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, SDL_Rect *
     } else {
         SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
     }
-    renderTexture(tex, ren, dst, clip);
+
+    SDL_RenderCopy(ren, tex, clip, &dst);
 }
